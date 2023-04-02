774865347e74d8ce3189fed6a49060bea9944a56
brandl <holgerbrandl@users.noreply.github.com>
2023-04-02 10:35:48 +0200

    added snake and camel case utils

--- src/main/kotlin/com/github/holgerbrandl/kdfutils/Misc.kt
+++ src/main/kotlin/com/github/holgerbrandl/kdfutils/Misc.kt
@@ -1,11 +1,36 @@
+@file:Suppress("unused")
+
 package com.github.holgerbrandl.kdfutils
 
 import org.apache.commons.csv.CSVFormat
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.into
+import org.jetbrains.kotlinx.dataframe.api.rename
 import org.jetbrains.kotlinx.dataframe.io.writeCSV
 import java.io.File
 
 
 //fun DataFrame.convert(val columnName, fal )
 
-fun org.jetbrains.kotlinx.dataframe.DataFrame<*>.writeTSV(file: File) = writeCSV(file, CSVFormat.TDF)
+fun DataFrame<*>.writeTSV(file: File) = writeCSV(file, CSVFormat.TDF)
 
+
+//https://stackoverflow.com/questions/60010298/how-can-i-convert-a-camel-case-string-to-snake-case-and-back-in-idiomatic-kotlin
+internal fun String.camelToSnakeCase(): String {
+    return "(?<=[a-zA-Z])[A-Z]".toRegex()
+        .replace(this) {
+            "_${it.value}"
+        }
+        .lowercase()
+}
+
+fun String.snakeToCamelCase(): String {
+    return "_[a-zA-Z]".toRegex().replace(this) {
+        it.value.replace("_","")
+            .uppercase()
+    }
+}
+
+/** Convert column names to snake case */
+fun DataFrame<*>.renameToSnakeCase() = rename { all() }.into { it.name.camelToSnakeCase() }
+fun DataFrame<*>.renameToCamelCase() = rename { all() }.into { it.name.snakeToCamelCase() }
